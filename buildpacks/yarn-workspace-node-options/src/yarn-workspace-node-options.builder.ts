85a57a0a5ee8552f43936eb202d623a047001dc8
Andrey Linko <AndreyLinko@gmail.com>
2021-12-28 17:14:26 +0000

    feat: env

--- buildpacks/yarn-workspace-node-options/src/yarn-workspace-node-options.builder.ts
+++ buildpacks/yarn-workspace-node-options/src/yarn-workspace-node-options.builder.ts
@@ -1,3 +1,8 @@
+import { PortablePath } from '@yarnpkg/fslib'
+import { execUtils }    from '@yarnpkg/core'
+import { xfs }          from '@yarnpkg/fslib'
+import { ppath }        from '@yarnpkg/fslib'
+
 import { Builder }      from '@monstrs/buildpack-core'
 import { BuildContext } from '@monstrs/buildpack-core'
 import { BuildResult }  from '@monstrs/buildpack-core'
@@ -11,5 +16,16 @@ export class YarnWorkspaceNodeOptionsBuilder implements Builder {
     if (nodeOptions) {
       ctx.addLaunchEnv('NODE_OPTIONS', nodeOptions)
     }
+
+    const layer = ctx.layers.get('node-options', true, true, true)
+
+    //await xfs.mkdirPromise(ppath.join(layer.path as PortablePath, 'env' as PortablePath))
+    //if (!(await xfs.existsPromise(ppath.join(layer.path as PortablePath, 'env.launch' as PortablePath)))) {
+    //  await xfs.mkdirPromise(ppath.join(layer.path as PortablePath, 'env.launch' as PortablePath))
+    //}
+
+    //await xfs.writeFilePromise(ppath.join(layer.path as PortablePath, 'env/NODE_OPTIONS' as PortablePath), nodeOptions)
+    //await xfs.writeFilePromise(ppath.join(layer.path as PortablePath, 'env/PWD' as PortablePath), '/workspace/operators/preview-operator')
+    //await xfs.writeFilePromise(ppath.join(layer.path as PortablePath, 'env.launch/NODE_OPTIONS' as PortablePath), nodeOptions)
   }
 }

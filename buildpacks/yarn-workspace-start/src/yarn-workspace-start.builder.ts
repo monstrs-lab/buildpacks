6751b9ac53c4623e427d4e636c1a1a8f5f117de6
Andrey Linko <AndreyLinko@gmail.com>
2022-01-03 21:10:53 +0300

    feat: yarn start withouth path

--- buildpacks/yarn-workspace-start/src/yarn-workspace-start.builder.ts
+++ buildpacks/yarn-workspace-start/src/yarn-workspace-start.builder.ts
@@ -3,8 +3,6 @@ import { writeFile }    from 'node:fs/promises'
 import { chmod }        from 'node:fs/promises'
 import { join }         from 'node:path'
 
-import execa            from 'execa'
-
 import { Builder }      from '@monstrs/libcnb'
 import { BuildContext } from '@monstrs/libcnb'
 import { BuildResult }  from '@monstrs/libcnb'
@@ -12,20 +10,13 @@ import { Process }      from '@monstrs/libcnb'
 
 export class YarnWorkspaceStartBuilder implements Builder {
   async build(ctx: BuildContext): Promise<BuildResult> {
-    const { stdout } = await execa('yarn', ['workspaces', 'list', '--json'])
+    const pkgjson = JSON.parse(readFileSync(join(ctx.applicationDir, 'package.json'), 'utf-8'))
 
-    const workspaces = stdout.split('\n').map((item) => JSON.parse(item))
-
-    const workspace = workspaces.find(({ name }) => name === process.env.WORKSPACE)
-
-    const pkgjson = JSON.parse(readFileSync(join(workspace.location, 'package.json'), 'utf-8'))
-
-    const { location } = workspace
     const command = pkgjson.scripts.start
 
     const result = new BuildResult()
 
-    await writeFile('/workspace/run.sh', `#!/usr/bin/env bash\ncd ${location} && ${command}`)
+    await writeFile('/workspace/run.sh', `#!/usr/bin/env bash\n${command}`)
     await chmod('/workspace/run.sh', '755')
 
     result.launchMetadata.processes.push(new Process('web', './run.sh', [], true, true))

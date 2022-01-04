c6816503e61706d848821bd49e47451907f7272f
Andrey Linko <AndreyLinko@gmail.com>
2022-01-04 12:40:26 +0300

    feat: start buildpack export layer

--- buildpacks/yarn-workspace-start/src/yarn-workspace-start.builder.ts
+++ buildpacks/yarn-workspace-start/src/yarn-workspace-start.builder.ts
@@ -13,22 +13,22 @@ export class YarnWorkspaceStartBuilder implements Builder {
     const pkgjson = JSON.parse(readFileSync(join(ctx.applicationDir, 'package.json'), 'utf-8'))
 
     const command = pkgjson.scripts.start
-    const nodeOptions = ['--require', join(ctx.applicationDir, '.pnp.cjs')].join(' ')
 
-    const result = new BuildResult()
-
-    await writeFile('/workspace/run.sh', `#!/usr/bin/env bash\nexport NODE_OPTIONS="${nodeOptions}"\n${command}`)
+    await writeFile('/workspace/run.sh', `#!/usr/bin/env bash\n${command}`)
     await chmod('/workspace/run.sh', '755')
-
-    result.launchMetadata.processes.push(new Process('web', './run.sh', [], true, true))
 
     const nodeOptionsLayer = await ctx.layers.get('node-options', true, true, true)
 
     nodeOptionsLayer.launchEnv.append(
       'NODE_OPTIONS',
-      nodeOptions,
+      ['--require', join(ctx.applicationDir, '.pnp.cjs')].join(' '),
       ' '
     )
+
+    const result = new BuildResult()
+
+    result.launchMetadata.processes.push(new Process('web', './run.sh', [], true, true))
+    result.layers.push(nodeOptionsLayer)
 
     return result
   }

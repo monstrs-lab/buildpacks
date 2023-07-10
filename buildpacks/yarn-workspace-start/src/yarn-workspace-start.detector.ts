f77f4b873c799e8948a3a2cdf2bbdf4d6513406a
monstrs <monstrs@users.noreply.github.com>
2023-07-10 21:25:24 +0000

    feat: update tools

--- buildpacks/yarn-workspace-start/src/yarn-workspace-start.detector.ts
+++ buildpacks/yarn-workspace-start/src/yarn-workspace-start.detector.ts
@@ -4,7 +4,7 @@ import type { DetectContext } from '@monstrs/libcnb'
 import { DetectResult }       from '@monstrs/libcnb'
 
 export class YarnWorkspaceStartDetector implements Detector {
-  async detect(ctx: DetectContext): Promise<DetectResult> {
+  async detect(_: DetectContext): Promise<DetectResult> {
     const result = new DetectResult()
 
     result.passed = true

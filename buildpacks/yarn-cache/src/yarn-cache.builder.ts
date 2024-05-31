4e4c38c80897965c0e8f8ce3081075e3c80ea450
Andrey Linko <AndreyLinko@gmail.com>
2024-05-31 14:49:39 +0000

    feat: update cache buildpack

--- buildpacks/yarn-cache/src/yarn-cache.builder.ts
+++ buildpacks/yarn-cache/src/yarn-cache.builder.ts
@@ -2,22 +2,17 @@ import type { Builder }      from '@monstrs/libcnb'
 import type { BuildContext } from '@monstrs/libcnb'
 
 import { readFile }          from 'node:fs/promises'
-import { readdir }           from 'node:fs/promises'
-import { copyFile }          from 'node:fs/promises'
-import { rmdir }             from 'node:fs/promises'
-import { writeFile }         from 'node:fs/promises'
+import { rename }            from 'node:fs/promises'
+import { link }              from 'node:fs/promises'
 import { join }              from 'node:path'
-import { relative }          from 'node:path'
 
 import { createHash }        from 'crypto'
-import { execa }             from 'execa'
-import YAML                  from 'yaml'
 
 import { BuildResult }       from '@monstrs/libcnb'
 
 export class YarnCacheBuilder implements Builder {
   async build(ctx: BuildContext): Promise<BuildResult> {
-    const yarnCachePath = join(ctx.applicationDir, '.yarn/cache')
+    const yarnCachePath = join(ctx.applicationDir, '.yarn/packages')
 
     const yarnLock = await readFile(join(ctx.applicationDir, 'yarn.lock'), 'utf8')
     const yarnLockCheckSum = createHash('md5').update(yarnLock).digest('hex')
@@ -25,34 +20,11 @@ export class YarnCacheBuilder implements Builder {
     const cacheLayer = await ctx.layers.get('yarn-cache', true, true, true)
 
     if (yarnLockCheckSum !== cacheLayer.getMetadata('locksum')) {
-      for await (const file of await readdir(yarnCachePath)) {
-        await copyFile(join(cacheLayer.path, file), join(yarnCachePath, file))
-      }
+      await rename(yarnCachePath, cacheLayer.path)
+      await link(cacheLayer.path, yarnCachePath)
 
       cacheLayer.setMetadata('locksum', yarnLockCheckSum.toString())
     }
-
-    await rmdir(yarnCachePath, { recursive: true })
-
-    const yarnrc = await readFile(join(ctx.applicationDir, '.yarnrc.yml'))
-
-    const yarnrcContent = YAML.parse(yarnrc.toString())
-
-    await writeFile(
-      join(ctx.applicationDir, '.yarnrc.yml'),
-      YAML.stringify({
-        ...yarnrcContent,
-        cacheFolder: relative(ctx.applicationDir, cacheLayer.path),
-      })
-    )
-
-    await execa('yarn', ['install', '--immutable'], {
-      cwd: ctx.applicationDir,
-      stdin: process.stdin,
-      stdout: process.stdout,
-      stderr: process.stderr,
-      env: process.env,
-    })
 
     return new BuildResult([cacheLayer])
   }
--- buildpacks/yarn-install/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { run }                 from '@monstrs/libcnb'
-
-import { YarnInstallBuilder }  from './yarn-install.builder.js'
-import { YarnInstallDetector } from './yarn-install.detector.js'
-
-run(new YarnInstallDetector(), new YarnInstallBuilder())
